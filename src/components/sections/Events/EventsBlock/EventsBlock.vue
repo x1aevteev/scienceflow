4423ce672db8af7416836e0c3b0005ee922479e8
Евтеев Артём Сергеевич <xlaevteev@bmstu.ru>
2024-10-23 18:57:16 +0300

    feat: 1) vuetify connection; 2) events-block refactor

--- src/components/sections/Events/EventsBlock/EventsBlock.vue
+++ src/components/sections/Events/EventsBlock/EventsBlock.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import {pageStore} from "@/stores/MainStore";
 import {onMounted} from "vue";
+import {VCard, VCardActions, VBtn} from "vuetify/components";
+import Container from "@/components/base/Container/Container.vue";
 
 const store = pageStore()
 
@@ -11,16 +13,28 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="events-block">
-    <template v-for="(event, i) in store.events.sections">
-      <div class="event">
-        <p>{{event.name}} {{event.slug}}</p>
-        <RouterLink :to="`/events/${event.slug}`">
-          Подробнее
-        </RouterLink>
-      </div>
-    </template>
-  </div>
+    <Container>
+      <VContainer>
+        <VRow>
+          <template v-for="(event, i) in store.events.sections">
+            <div class="event">
+              <VCol>
+                <VSheet>
+                  <VCard title="Card title" subtitle="Subtitle" :text="event.name">
+                    <VCardActions>
+                      <RouterLink :to="`/events/${event.slug}`">
+                        <VBtn>Подробнее</VBtn>
+                      </RouterLink>
+                    </VCardActions>
+                  </VCard>
+                </VSheet>
+              </VCol>
+            </div>
+
+          </template>
+        </VRow>
+      </VContainer>
+    </Container>
 </template>
 
 <style scoped lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(vuetify)
 app.use(router);
 app.use(pinia);
 
